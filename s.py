7c762bd3534caf414087290e9846c788477f68b5
Cheshta17 <122281116+Cheshta17@users.noreply.github.com>
2024-06-20 00:25:05 +0530

    Create s.py

---
+++ s.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title ("Hello Tassanee*)
+st.header('Testing Streamlit')
+st.text("This is my first data app on Streamlit!")
+st.text_input('Fist name')
+st.number_input('Pick a number',0,10)
