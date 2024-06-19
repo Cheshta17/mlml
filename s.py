9e48bf1793275c64f1ff8aaf973a244c73b1e41f
Cheshta17 <122281116+Cheshta17@users.noreply.github.com>
2024-06-20 00:26:47 +0530

    Update s.py

--- s.py
+++ s.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title ("Hello Tassanee*)
+st.title ("Hello Tassanee")
 st.header('Testing Streamlit')
 st.text("This is my first data app on Streamlit!")
 st.text_input('Fist name')
